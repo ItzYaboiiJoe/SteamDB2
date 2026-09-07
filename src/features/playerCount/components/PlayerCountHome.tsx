cdf80fdfdaf0cb158713d0a813691891209e4e37
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2026-09-07 00:42:21 -0400

    Temporarily Added an iframe from steamdb as a placeholder

--- src/features/playerCount/components/PlayerCountHome.tsx
+++ src/features/playerCount/components/PlayerCountHome.tsx
@@ -4,6 +4,12 @@ const PlayerCountHome = ({ appId }: { appId: number }) => {
       <div className="flex w-full max-w-2xl flex-col items-center gap-6 text-center">
         <div className="space-y-2">
           <div>Player Count for App ID: {appId}</div>
+          <iframe
+            src={`https://steamdb.info/embed/?appid=${appId}`}
+            height="389"
+            width="1000"
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
     </div>
